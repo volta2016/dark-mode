f5ce0c3c04259a700c539323a05341da6330e082
Volta <giovoltadesign@gmail.com>
2020-06-12 01:24:08 -0400

    js complete darkmode

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-alert('hola mundo');
+const checkbox = document.querySelector('#checkbox');
+
+if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    checkbox.setAttribute('checked', true);
+}
+
+
+// document.body.style.setProperty('--cardGray', 'blue')
+
+checkbox.addEventListener('change', function(event)  {
+    if (this.checked) {
+        document.body.classList.remove('is-light-mode');
+        document.body.classList.add('is-dark-mode');
+    } else {
+        document.body.classList.remove('is-dark-mode');
+        document.body.classList.add('is-light-mode');
+    }
+})
